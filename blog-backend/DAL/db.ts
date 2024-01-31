e67303ed9bccda551c0da615dd8a88a9fabdac7a
Ronit Heinoch <ronit.heinoch@gmail.com>
2024-01-31 09:30:51 +0200

    remove InMemoryDB

--- blog-backend/DAL/db.ts
+++ blog-backend/DAL/db.ts
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: process.env.POSTGRES_CLOUD_USER,
     password: process.env.POSTGRES_CLOUD_PASS,
     host: process.env.POSTGRES_CLOUD_HOST,
-    database: process.env.POSTGRES_CLOUD_DB,
+    database: process.env.POSTGRES_CLOUD_DB_NAME,
     port: parseInt(process.env.POSTGRES_CLOUD_PORT!),
 
 });
